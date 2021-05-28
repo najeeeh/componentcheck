2deecb341a12af9c3197cfc84d8e717917e62d4a
Najeh Rahali <Rahali.najeeh@gmail.com>
2021-05-28 09:54:19 +0100

    first

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,7 @@
+export const MyAddress = () => {
+    return (
+      <>
+        <h2>Address : The village of the hidden leaf (Konoha) in the land of fire</h2>
+      </>
+    );
+   };
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,7 @@
+export const MyFullName = () => {
+    return (
+      <>
+        <h1>Full Name : Naruto Uzumaki</h1>
+      </>
+    );
+   };
